d49b6d81ef653e7535a5bbd0878ef31c7d80ca94
sanath rayala <57697459+SanathRayala@users.noreply.github.com>
2022-05-17 19:11:15 +0530

    website uplode

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
+let typed = new Typed(".dynamicText", {
+  strings: [
+    "Youtuber",
+    "Java Developer",
+    "Tech Enthusiast",
+    "TechTrainer",
+    "Motivational Speaker",
+    "Programmer",
+  ],
+  typeSpeed: 45,
+  backSpeed: 45,
+  loop: true,
+});
+
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
+
+function serviceNotAvailable() {
+  alert("Sorry for the inconvinience\nService not available as of now");
+}
